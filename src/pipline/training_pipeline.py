5df9f5b702770acc15c2772ad7aaf7fb7344d9ea
agarwalson02 <agarwalson02@gmail.com>
2026-03-19 00:18:05 +0530

    model trainer added

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -5,20 +5,24 @@ from src.logger import logging
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 from src.entity.artifact_entity import (DataIngestionArtifact,
                                         DataValidationArtifact,
-                                        DataTransformationArtifact)
+                                        DataTransformationArtifact,
+                                        ModelTrainerArtifact)
 
 from src.entity.config_entity import (DataIngestionConfig,
                                     DataValidationConfig,
-                                    DataTransformationConfig)
+                                    DataTransformationConfig,
+                                    ModelTrainerConfig)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config=DataIngestionConfig()
         self.data_validation_config=DataValidationConfig()
         self.data_transformation_config=DataTransformationConfig()
+        self.model_trainer_config=ModelTrainerConfig()
 
     def start_data_ingestion(self)->DataIngestionArtifact:
 
@@ -57,6 +61,18 @@ class TrainPipeline:
         except Exception as e:
             raise MyException(e,sys)
 
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
+        logging.info("Entered the start_model_trainig method of TrainPipeline class")
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys)
+
 
     def run_pipeline(self, )->None:
         try:
@@ -65,5 +81,6 @@ class TrainPipeline:
             data_transformation_artifact=self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact,data_validation_artifact=data_validation_artifact
             )
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
         except Exception as e:
             raise MyException(e,sys)
